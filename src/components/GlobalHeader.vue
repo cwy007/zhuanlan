7f572271052262c95c3928484b33095572a22376
Viking Zhang <vikingmute@gmail.com>
2020-07-05 21:23:24 +0800

    add Dropdown component, first part

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -6,14 +6,16 @@
       <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
     </ul>
     <ul v-else class="list-inline mb-0">
-      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">你好 {{user.name}}</a></li>
+      <li class="list-inline-item">
+        <dropdown :title="`你好 ${user.name}`"></dropdown>
+      </li>
     </ul>
   </nav>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
-
+import Dropdown from './Dropdown.vue'
 export interface UserProps {
   isLogin: boolean;
   name?: string;
@@ -21,6 +23,9 @@ export interface UserProps {
 }
 export default defineComponent({
   name: 'GlobalHeader',
+  components: {
+    Dropdown
+  },
   props: {
     user: {
       type: Object as PropType<UserProps>,
