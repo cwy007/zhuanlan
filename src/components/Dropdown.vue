672dcb417516f83be8f875c4638b6bfdc89fdf6f
Viking Zhang <vikingmute@gmail.com>
2020-07-07 10:28:37 +0800

    add Clickoutside part 1

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="dropdown">
+<div class="dropdown" ref="dropdownRef">
   <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">
     {{title}}
   </a>
@@ -10,7 +10,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue'
 export default defineComponent({
   name: 'Dropdown',
   props: {
@@ -21,12 +21,27 @@ export default defineComponent({
   },
   setup() {
     const isOpen = ref(false)
+    const dropdownRef = ref<null | HTMLElement>(null)
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
+    const handler = (e: MouseEvent) => {
+      if (dropdownRef.value) {
+        if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
+          isOpen.value = false
+        }
+      }
+    }
+    onMounted(() => {
+      document.addEventListener('click', handler)
+    })
+    onUnmounted(() => {
+      document.removeEventListener('click', handler)
+    })
     return {
       isOpen,
-      toggleOpen
+      toggleOpen,
+      dropdownRef
     }
   }
 })
