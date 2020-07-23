c9e12c5ddd6a888e77d94f0a202096fe422d990a
Viking Zhang <vikingmute@gmail.com>
2020-07-23 16:34:15 +0800

    add vue router beforeEach part 1

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import ColumnDetail from './views/ColumnDetail.vue'
 import CreatePost from './views/CreatePost.vue'
+import store from './store'
 const routerHistory = createWebHistory()
 const router = createRouter({
   history: routerHistory,
@@ -28,6 +29,13 @@ const router = createRouter({
       component: ColumnDetail
     }
   ]
+})
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'login' && !store.state.user.isLogin) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
 })
 
 export default router
