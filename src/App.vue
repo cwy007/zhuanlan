093f46a753bef9dbb6bf855a869ff1debf3fe514
Viking Zhang <vikingmute@gmail.com>
2020-09-19 15:37:19 +0800

    update footer info

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,11 @@
   <footer class="text-center py-4 text-secondary bg-light mt-auto">
   <small>
     <ul class="list-inline mb-0">
-      <li class="list-inline-item">© 2020 者也专栏</li>
-      <li class="list-inline-item">课程</li>
-      <li class="list-inline-item">文档</li>
-      <li class="list-inline-item">联系</li>
-      <li class="list-inline-item">更多</li>
+      <li class="list-inline-item">© 慕课网（imooc.com）版权所有 | 津ICP备20000929号-1</li>
+      <li class="list-inline-item"><a href="https://coding.imooc.com/class/449.html" target="_blank">购买课程</a></li>
+      <li class="list-inline-item"><a href="http://docs.vikingship.xyz/" target="_blank">文档</a></li>
+      <li class="list-inline-item"><a href="http://api.vikingship.xyz/" target="_blank">API 在线调试</a></li>
+      <li class="list-inline-item"><a href="http://showcase.vikingship.xyz/" target="_blank">组件库演示</a></li>
     </ul>
   </small>
 </footer>
