e807c65a3d9d1c0578a41afd06c741dc3504ea36
Viking Zhang <vikingmute@gmail.com>
2020-10-29 11:05:41 +0800

    update to bootstrap5 alpha2, modify some style changes, fix one typescript error

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -4,7 +4,7 @@
       <div class="card-body">
         <h4><router-link :to="`/posts/${post._id}/`">{{post.title}}</router-link></h4>
         <div class="row my-3 align-items-center">
-          <div v-if="post.image" class="col-4">
+          <div v-if="post.image && typeof post.image !== 'string'" class="col-4">
             <img :src="post.image.fitUrl" :alt="post.title" class="rounded-lg w-100">
           </div>
           <p :class="{'col-8': post.image}" class="text-muted">{{post.excerpt}}</p>
