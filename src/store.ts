dd30a628b0ccfcb72ef555e2b9c0f12fa1d1d569
Viking Zhang <vikingmute@gmail.com>
2020-11-20 20:17:26 +0800

    fix logout typo

--- src/store.ts
+++ src/store.ts
@@ -122,7 +122,7 @@ const store = createStore<GlobalDataProps>({
     logout(state) {
       state.token = ''
       state.user = { isLogin: false }
-      localStorage.remove('token')
+      localStorage.removeItem('token')
       delete axios.defaults.headers.common.Authorization
     }
   },
