dcb4b8518d150fac6ba8a3f856b2badab31664d7
chenweiyan <chanweiyan007@gmail.com>
2020-12-08 21:06:40 +0800

    add bootstrap style to ColumnList component

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,22 +1,26 @@
 <template>
-  <ul>
-    <li v-for="column in list" :key="column.id">
-      <img :src="column.avatar" :alt="column.title">
-      <h5>{{ column.title }}</h5>
-      <p>{{ column.description }}</p>
-      <a href="#">进入专栏</a>
-    </li>
-  </ul>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img  :src="column.avatar" :alt="column.title" class="rounded-circle border border-light w-25 my-3" >
+          <h5 class="card-title">{{column.title}}</h5>
+          <p class="card-text text-left">{{column.description}}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 // 这里要指定为 lang="ts"
 
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, computed } from 'vue'
 export interface ColumnProps {
   id: number;
   title: string;
-  avatar: string;
+  avatar?: string; // 将图片改为可选
   description: string;
 }
 
@@ -24,8 +28,21 @@ export default defineComponent({
   name: 'ColumnList',
   props: {
     list: {
-      type: Array as PropType<ColumnProps[]>,
+      type: Array as PropType<ColumnProps[]>, // 断言
       required: true
+    }
+  },
+  setup (props) {
+    const columnList = computed(() => {
+      return props.list.map(column => {
+        if (!column.avatar) {
+          column.avatar = require('@/assets/column.jpg')
+        }
+        return column
+      })
+    })
+    return {
+      columnList
     }
   }
 })
