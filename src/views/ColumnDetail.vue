3e2b84edb7ce9d3f39b303195eb801461e2f9e98
chenweiyan <chanweiyan007@gmail.com>
2020-12-09 20:25:16 +0800

    add testData and style ColumnDetail

--- src/views/ColumnDetail.vue
+++ src/views/ColumnDetail.vue
@@ -1,18 +1,37 @@
 <template>
-  <div>
-    <pre>{{route}}</pre>
+  <div class="column-detail-page w-75 mx-auto">
+    <div class="column-info row mb-4 border-bottom pb-4 align-items-center" v-if="column">
+      <div class="col-4 text-center">
+        <img :src="column.avatar" :alt="column.title" class="rounded-circle border">
+      </div>
+      <div class="col-8">
+        <h4>{{column.title}}</h4>
+        <p class="text-muted">{{column.description}}</p>
+      </div>
+    </div>
   </div>
+  <post-list :list="list"></post-list>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useRoute } from 'vue-router'
+import { testData, testPosts } from '@/testData'
+import PostList from '@/components/PostList.vue'
 
 export default defineComponent({
+  components: {
+    PostList
+  },
   setup () {
     const route = useRoute()
+    const currentId = +route.params.id
+    const column = testData.find(c => c.id === currentId)
+    const list = testPosts.filter(post => post.columnId === currentId)
     return {
-      route
+      route,
+      column,
+      list
     }
   }
 })
