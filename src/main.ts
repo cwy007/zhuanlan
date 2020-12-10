87278416730290874a155a3a8d432cf6e6c3a6ba
chenweiyan <chanweiyan007@gmail.com>
2020-12-10 17:00:01 +0800

    install axios & test imooc api

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,21 @@ import { createApp } from 'vue'
 import router from '@/router'
 import store from '@/store'
 import App from './App.vue'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://apis.imooc.com/api/'
+axios.interceptors.request.use(config => {
+  config.params = { ...config.params, icode: 'A51C3975141CD028' }
+  if (config.data instanceof FormData) {
+    config.data.append('icode', 'A51C3975141CD028')
+  } else {
+    config.data = { ...config.data, icode: 'A51C3975141CD028' }
+  }
+  return config
+})
+axios.get('/columns').then((res) => {
+  console.log(res.data)
+})
 
 const app = createApp(App)
 app.use(router)
