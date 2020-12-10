53765aea4920d18082a562f0f35ef2b023b3a029
chenweiyan <chanweiyan007@gmail.com>
2020-12-10 15:39:36 +0800

    add meta data to routes && use meta data to implement route authentication

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,7 +17,8 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      meta: { redirectAlreadyLogin: true }
     },
     {
       path: '/column/:id',
@@ -27,14 +28,17 @@ const router = createRouter({
     {
       path: '/create',
       name: 'create',
-      component: CreatePost
+      component: CreatePost,
+      meta: { requiredLogin: true }
     }
   ]
 })
 
 router.beforeEach((to, from, next) => {
-  if (to.name !== 'login' && !store.state.user.isLogin) {
+  if (!store.state.user.isLogin && to.meta.requiredLogin) {
     next({ name: 'login' })
+  } else if (store.state.user.isLogin && to.meta.redirectAlreadyLogin) {
+    next('/')
   } else {
     next()
   }
